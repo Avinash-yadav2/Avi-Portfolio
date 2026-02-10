fb8bebd23ef287ff41037dee7a0c7f087a1979a1
Avinash Yadav <avinashyadavsonu71@gmail.com>
2026-02-10 14:52:47 +0530

    Fixed API URL for Production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
-// Backend URL
-export const BASE_URL = 'http://localhost:5000'; 
+//Dynamic URL 
+
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'; 
 
 // Axios Instance
 const API = axios.create({
   baseURL: `${BASE_URL}/api`,
+});
+
+//Auth Token Logic ---
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('token')) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  }
+  return req;
 });
 
 export default API;
